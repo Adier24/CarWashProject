1892c7eadc82433ce3d7bba4b8a1cd0241b4d50c
Adier24 <amaldonado1079@arecibointer.edu>
2023-10-11 11:25:49 -0400

    CarWashProject

---
+++ JavaScripTest/ejemplo/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    $('#btnCambiar').click(function () {
+
+    $('h1').css('color', 'red');
+    $('h1').css('background-color', 'gold');
+    $('h1').css('font-size', '5em');
+    $('h1').css('font-family', 'Arial, Helvatica, sans-serif');
+
+    $('p').css('color', 'blue');
+    $('p').css('font-size', '34px');
+});
+}); 
